afce1e0e90d7325a132c4709009de317973fe71c
akkas_dev <codersadhin@gmail.com>
2024-05-29 13:31:00 +0800

    max min value by using selection array

---
+++ sort_using_selection.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+    public:
+        string name;
+        int roll;
+        int marks;
+};
+
+int main()
+{
+    int n;
+    cin>>n;
+    Student arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i].name>>arr[i].roll>>arr[i].marks;
+    }
+
+    cout<<"max"<<endl;
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            if(arr[i].marks > arr[j].marks){
+                swap(arr[i],arr[j]);
+            }
+        }
+        
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i].name<<" "<<arr[i].roll<<" "<<arr[i].marks<<endl;
+    }
+
+    cout<<"min"<<endl;
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            if(arr[i].marks < arr[j].marks){
+                swap(arr[i],arr[j]);
+            }
+        }
+        
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i].name<<" "<<arr[i].roll<<" "<<arr[i].marks<<endl;
+    }
+    
+    
+    return 0;
+}
